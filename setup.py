bf10399365caf14e21ae757e1f4b36371990050e
lahari-1564 <lahari.battu@fau.de>
2024-11-10 22:38:07 +0100

    made changes in the code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Homework_2',
+    version='1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='lalit',
+    author_email='',
+    description=''
+)
